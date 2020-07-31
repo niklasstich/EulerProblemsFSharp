3c7a284cc28c1ae0d70a2b1c817077fec92b440e
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-07-31 05:15:50 +0200

    tweak solution to Problem1

--- Solutions1-10/Problem0001.fsx
+++ Solutions1-10/Problem0001.fsx
@@ -1,10 +1,9 @@
 let Problem1 =
     let fizzbuzz len =
-        0
-        |> Seq.unfold (fun state ->
+        Seq.unfold (fun state ->
             if (state >= len) then None
             else if (state % 3 = 0 || state % 5 = 0) then Some(state, state + 1)
-            else Some(0, state + 1))
+            else Some(0, state + 1)) 0
     fizzbuzz 1000 |> Seq.sum
 
 let Problem1_1 =
